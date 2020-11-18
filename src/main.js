49a24c9e460fedcb1df143aa23cb5f87e3ed97f4
Blede <maxtergameplay@gmail.com>
2020-11-17 21:50:20 -0500

    Update main.js
    Being able to overwrite materialize css easier

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import 'materialize-css/dist/css/materialize.min.css'
+import 'material-design-icons/iconfont/material-icons.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'material-design-icons/iconfont/material-icons.css'
+
 
 
 Vue.config.productionTip = false
